f47378a96274e31850a76e76a56fef64d9534087
Chidinma Nze <chidinma.ogbuaku@gmail.com>
2020-11-06 20:35:51 -0500

    work in progress

--- src/components/Employees/index.js
+++ src/components/Employees/index.js
@@ -1,43 +1,48 @@
 import React from "react";
 import "./style.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import employees from "../../employees.json";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class Employees extends React.Component {
-constructor(props) {
-super(props);
-
-  const { employees } = props;
-  console.log (props, "");
-  
-  return (
-    <table className="table">
-  <thead>
-    <tr>
-      <th scope="col"><button onClick={e => props.sortBy(e, 'id')}>
-      Employee ID </button></th> 
-      <th scope="col">First Name</th> 
-      <th scope="col">Last Name</th>
-      <th scope="col">Role</th>
-      <th scope="col">Email</th>
-      <th scope="col">Phone</th>
-    </tr>
-  </thead>
-  <tbody>
+  constructor(props) {
+    super(props);
     
-  {employees.map(employee => (
-    <tr key={employee.id}>
-      <td>{employee.id}</td> 
-      <td>{employee.firstname}</td> 
-      <td>{employee.lastname}</td> 
-      <td>{employee.role}</td> 
-      <td>{employee.email}</td> 
-      <td>{employee.phone}</td> 
-    </tr>
-  ))}
-  </tbody>
-</table>
-  );
-}
+    const { employees } = props;
+    console.log(props, "");
+  }
+    render() {
+      return (
+        <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">
+              <button onClick={(e) => props.sortBy(e, "id")}>
+                Employee ID{" "}
+              </button>
+            </th>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Role</th>
+            <th scope="col">Email</th>
+            <th scope="col">Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.map((employee) => (
+            <tr key={employee.id}>
+              <td>{employee.id}</td>
+              <td>{employee.firstname}</td>
+              <td>{employee.lastname}</td>
+              <td>{employee.role}</td>
+              <td>{employee.email}</td>
+              <td>{employee.phone}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+
 }
 
 export default Employees;
